a21fa062810f137dcadf0112dc0569cb91a402c6
peter <petecruckshank@gmail.com>
2018-11-04 20:16:04 -0500

    the config file for dev & deploy + MongoDB URI

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.BASE_URL || 3000,
+    URL: process.env.PORT || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://peter:<dbpassword>@ds259802.mlab.com:59802/customer_api'
+}
