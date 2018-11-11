f95f879b92c2f991c6e727a2f6bff787bede6009
peter <petecruckshank@gmail.com>
2018-11-10 21:05:30 -0500

    formatted doc with Prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const restify = require('restify');
-const mongoose = require('mongoose');
-const config = require('./config');
+const restify = require("restify");
+const mongoose = require("mongoose");
+const config = require("./config");
 
 const server = restify.createServer();
 
@@ -8,16 +8,19 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 server.listen(config.PORT, () => {
-    mongoose.set('useFindAndModify', false);
-    mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true});
+  mongoose.set("useFindAndModify", false);
+  mongoose.connect(
+    config.MONGODB_URI,
+    { useNewUrlParser: true }
+  );
 });
 
 const db = mongoose.connection;
 
-db.on('error', (err) => console.warn(err));
+db.on("error", err => console.warn(err));
 
-db.once('open', () => {
-    require('./routes/customers')(server);
-    require('./routes/users')(server);
-    console.log(`Server started on port ${config.PORT}`);
+db.once("open", () => {
+  require("./routes/customers")(server);
+  require("./routes/users")(server);
+  console.log(`Server started on port ${config.PORT}`);
 });
