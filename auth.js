374bf3da6327107788797bb98e75a63ad8e10416
peter <petecruckshank@gmail.com>
2018-11-11 21:24:37 -0500

    changed code to match Brad's repo

--- auth.js
+++ auth.js
@@ -1,26 +1,22 @@
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 exports.authenticate = (email, password) => {
-    return new Promise( async (resolve, reject) => {
-        try {
-            // Get user by email
-            const user = await User.findOne({email});
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Get user by email
+      const user = await User.findOne({ email });
 
-            // Match password
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-                if (err) throw err;
-                if (isMatch) {
-                    resolve(user);
-                } else {
-                    // Password didn't match
-                    reject('Authentication Failed');
-                }
-            });
-        } catch (err) {
-            // Email not found
-            reject('Authentication Failed');
-        }
-    });
+      // Match password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) throw err;
+        if (!isMatch) throw "Password did not match";
+        resolve(user);
+      });
+    } catch (err) {
+      // Email not found
+      reject("Authentication Failed");
+    }
+  });
 };
